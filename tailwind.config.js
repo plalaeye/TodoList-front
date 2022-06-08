4b9acb6b90364351f630376ab62c436bb50a7a64
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-08 10:11:41 +0700

    💄 Add color and font to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          1: '#FCF8EC',
+          2: '#E6F0EF',
+          3: '#79A3B1',
+          4: '#456268',
+          red: '#D43C3C',
+          green: '#469451',
+        },
+        dark: {
+          1: '#303841',
+          2: '#3A4750',
+          3: '#F6C90E',
+          4: '#EEEEEE',
+          red: '#FF7979',
+          green: '#74D082',
+        },
+      },
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
