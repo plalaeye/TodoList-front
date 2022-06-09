060f64d2860ea50d847c3ef9985ad296bb2764a2
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-09 17:07:04 +0700

    🚧 Fix `Login` component corresponding to other components

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,10 +1,9 @@
-import { useState } from "react"
-import Button from "./Button"
-import Input from "./Input"
+import { useState } from 'react'
+import Button from './Button'
+import Input from './Input'
 
 const Login = () => {
-
-	const [name, setName] = useState('')
+  const [name, setName] = useState('')
 
   const onClick = () => {
     console.log('click')
@@ -17,16 +16,20 @@ const Login = () => {
 
   return (
     <>
-			<div className="h-screen flex justify-center items-center bg-light-1">
-				<div className="flex-column w-fit h-fit justify-items-center text-center space-y-6 p-10 bg-light-2 rounded-2xl shadow-lg">
-					<h1 className="font-bold text-light-4 text-2xl">
-						Please enter your name
-					</h1>
-					<Input type="text" placeholder="something" onChange={onChange} />
-					<Button onClick={onClick}>Eiei</Button>
-				</div>
-			</div>
-		</>
+      <div className="h-screen flex justify-center items-center bg-light-1 dark:bg-dark-1">
+        <div className="flex-column w-fit h-fit justify-items-center text-center space-y-6 p-10 bg-light-2 dark:bg-dark-2 rounded-2xl shadow-lg">
+          <h1 className="font-bold text-light-4 dark:text-dark-4 text-2xl">
+            Please enter your name
+          </h1>
+          <Input type="text" placeholder="something" onChange={onChange} />
+          <Button
+            onClick={onClick}
+            text="Continue"
+            icon="majesticons:login-line"
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
