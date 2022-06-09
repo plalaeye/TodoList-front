e08a596eac34ee0c98615fb0a40fb61d74de72c7
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-09 17:07:45 +0700

    🚧 Fix login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,11 @@
-import classNames from 'classnames'
-import { useState } from 'react'
-import Button from '../components/Button'
-import Input from '../components/Input'
 import Login from '../components/Login'
-import Theme from '../components/Theme'
+import ThemeToggle from '../components/ThemeToggle'
+import userStoreInstance from '../contexts/UserStore'
 
 const index = () => {
   return (
     <>
-      <Theme />
+      <ThemeToggle userStore={userStoreInstance} />
       <Login />
     </>
   )
