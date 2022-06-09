c6678b89683544427e67c4ee3854ecb33e8d19be
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-09 17:06:27 +0700

    🚧 Add dark mode to `Input` component

--- components/Input.tsx
+++ components/Input.tsx
@@ -7,7 +7,7 @@ export interface IInputProps {
 const Input = ({ type, placeholder, onChange }: IInputProps) => {
   return (
     <input
-      className="border-light-4 border-2 text-size-xl rounded-lg w-full px-2.5"
+      className="border-light-4 dark:border-dark-4 bg-light-2 dark:bg-dark-2 border-2 text-light-4 dark:text-dark-4 text-size-xl rounded-lg w-full px-2.5 py-1"
       type={type}
       placeholder={placeholder}
       onChange={(e) => onChange(e)}
