5b47cc0c6c861d51ab380a8c261fd4f28e62a37a
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-09 14:23:26 +0700

    🚧 Create login page

---
+++ pages/login.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames'
+import { useState } from 'react'
+import Button from '../components/Button'
+import Input from '../components/Input'
+import Login from '../components/Login'
+import Theme from '../components/Theme'
+
+const index = () => {
+  return (
+    <>
+      <Theme />
+      <Login />
+    </>
+  )
+}
+
+export default index
