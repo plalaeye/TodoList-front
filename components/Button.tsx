fbe725006f45c3ebfd70d36e055ebd6d20d25e7a
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-09 17:05:55 +0700

    🚧 Add text and icon to `Button` component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,21 @@
-import classNames from 'classnames'
+import { Icon } from '@iconify/react'
 
 export interface IButtonProps {
   onClick: () => void
-  children: React.ReactNode
+  text: string
+  icon: string
 }
 
-const Button = ({ onClick, children }: IButtonProps) => {
+const Button = ({ onClick, text, icon }: IButtonProps) => {
   return (
     <button
-      className='m-auto bg-light-4 py-4 px-5 rounded-lg text-light-1 text-size-xl'
+      className="m-auto bg-light-4 dark:bg-dark-3 py-4 px-5 rounded-lg text-light-1 dark:text-dark-1 text-size-xl"
       onClick={onClick}
     >
-      {children}
+      <div className="flex">
+        <h3>{text}</h3>
+        <Icon icon={icon} className="text-2xl" />
+      </div>
     </button>
   )
 }
