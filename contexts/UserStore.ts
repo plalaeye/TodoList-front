4be877151be203d5ce86f7cca0ede19cafe1c491
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-15 09:52:59 +0700

    🚧 Create `Store` for web information

--- contexts/UserStore.ts
+++ contexts/UserStore.ts
@@ -1,71 +1,108 @@
-import { action, makeObservable, observable } from 'mobx'
+import { action, flow, makeObservable, observable } from 'mobx'
+import { addCategory } from '../api/CategoryFunction'
+import {
+  createTask,
+  deleteTask,
+  updateStatus,
+  updateTask,
+} from '../api/TaskFunction'
+import { themeChange } from '../api/ThemeFunction'
 import { TaskStatus } from '../enum/taskStatus'
 import { Theme } from '../enum/theme'
+import CreateTaskDto from '../interfaces/CreateTaskDto'
+import ITask from '../interfaces/Task'
+import { IUser } from '../interfaces/User'
 import { setDarkTheme, setLightTheme } from '../util/setTheme'
-
-export interface task {
-  _id?: string
-  title: string
-  detail: string
-  dueDate?: Date
-  category?: string
-  tags?: string[]
-  status?: TaskStatus
-}
 
 export class userStore {
   _id: string = ''
   name: string = ''
-  tasks: task[] = []
+  category: string[] = []
+  tasks: ITask[] = []
   theme: Theme = Theme.LIGHT
+  selectedCategory: string = 'All Tasks'
+  selectedTask: string = ''
 
   constructor() {
     makeObservable(this, {
       _id: observable,
       name: observable,
+      category: observable,
       tasks: observable,
       theme: observable,
-      createUser: action,
-      setTheme: action,
-      createTask: action,
-      updateTask: action,
-      deleteTask: action,
+      selectedCategory: observable,
+      selectedTask: observable,
+      setTheme: flow,
+      addCategory: flow,
+      createTask: flow,
+      updateTask: flow,
+      updateStatus: flow,
+      deleteTask: flow,
+      setCategory: action,
+      setTask: action,
     })
   }
 
-  createUser({
-    _id,
-    name,
-    tasks,
-    theme,
-  }: {
-    _id: string
-    name: string
-    tasks: task[]
-    theme: Theme
-  }) {
-    this._id = _id
-    this.name = name
-    this.tasks = tasks
-    this.theme = theme
+  setUser(user: IUser) {
+    this._id = user._id
+    this.name = user.name
+    this.category = user.category
+    this.tasks = user.tasks
+    this.setTheme(user.theme)
   }
 
-  setTheme(theme: Theme) {
+  *setTheme(theme: Theme) {
     this.theme = theme
     if (theme === Theme.DARK) setDarkTheme()
     else setLightTheme()
+    yield themeChange(theme)
   }
 
-  createTask(task: task) {
-    this.tasks.push(task)
+  *addCategory(category: string) {
+    this.category.push(category)
+    yield addCategory(category)
   }
 
-  updateTask(task: task) {
+  setCategory(category: string) {
+    this.selectedCategory = category
+    let curTaskCat = this.tasks.find(
+      (task) => task._id === this.selectedTask
+    )?.category
+    if (
+      curTaskCat &&
+      this.selectedCategory !== 'All Tasks' &&
+      this.selectedCategory !== curTaskCat
+    ) {
+      this.selectedTask = ''
+    }
+  }
+
+  setTask(taskId: string) {
+    this.selectedTask = taskId
+  }
+
+  *createTask(task: CreateTaskDto) {
+    const user: IUser = yield createTask(task)
+    this.setUser(user)
+  }
+
+  *updateTask(task: ITask) {
     this.tasks = this.tasks.map((t) => (t._id === task._id ? task : t))
+    yield updateTask(task)
   }
 
-  deleteTask(taskId: string) {
+  *updateStatus(taskId: string, status: TaskStatus) {
+    this.tasks = this.tasks.map((task) =>
+      task._id === taskId ? { ...task, status } : task
+    )
+    yield updateStatus(taskId, status)
+    if (taskId === this.selectedTask) this.setTask('')
+  }
+
+  *deleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task._id !== taskId)
+    yield deleteTask(taskId)
+    if (taskId === this.selectedTask) this.setTask('')
   }
 }
 
