219e492268d53099cf6bf4a8048058b7df26cd41
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-15 09:56:05 +0700

    🚧 Create `LogoutButton` for sidebar

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,24 @@
+import { Icon } from '@iconify/react'
+import Router from 'next/router'
+import { logout } from '../api/AccountFunction'
+
+const LogoutButton = () => {
+  const onClick = async () => {
+    await logout()
+    Router.push('/login')
+  }
+
+  return (
+    <button
+      className="w-full h-fit bg-light-3 dark:bg-dark-2 py-4 px-5 rounded-lg text-light-1 dark:text-dark-4 text-size-xl shadow-md hover:bg-light-4 hover:dark:bg-dark-3 hover:dark:text-dark-1"
+      onClick={onClick}
+    >
+      <div className="flex justify-center items-center space-x-1.5">
+        <Icon icon="majesticons:logout-line" hFlip={true} className="" />
+        <h3>Logout</h3>
+      </div>
+    </button>
+  )
+}
+
+export default LogoutButton
