215f7f014e6c494c54f1a9c291d8dd3095bfb352
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-15 23:14:57 +0700

    🚧 Make `Task` double-clickable

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames'
 import { observer } from 'mobx-react'
+import addTaskPopupStoreInstance from '../contexts/AddTaskPopupStore'
+import popupStoreInstance from '../contexts/PopupStore'
 import userStoreInstance from '../contexts/UserStore'
 import ITask from '../interfaces/Task'
 import Checkbox from './Checkbox'
@@ -10,23 +12,55 @@ export interface ITaskProps {
 }
 
 const Task = observer(({ task }: ITaskProps) => {
+  let delay = 200
+  let prevent = false
+
   const onClick = () => {
     userStoreInstance.setTask(task._id)
+  }
+
+  const onDoubleClick = () => {
+    userStoreInstance.setTask(task._id)
+    const date = new Date(task.dueDate as Date)
+    const dateString = task.dueDate
+      ? date.toISOString().split('T')[0]
+      : undefined
+
+    addTaskPopupStoreInstance.setId(task._id)
+    addTaskPopupStoreInstance.setTitle(task.title)
+    addTaskPopupStoreInstance.setDueDate(dateString)
+    addTaskPopupStoreInstance.setDetail(task.detail)
+    addTaskPopupStoreInstance.setEdit()
+    popupStoreInstance.openAddTask()
+  }
+
+  const handleClick = () => {
+    setTimeout(function () {
+      if (!prevent) {
+        onClick()
+      }
+      prevent = false
+    }, delay)
+  }
+
+  const handleDoubleClick = () => {
+    prevent = true
+    onDoubleClick()
   }
 
   const selected = task._id === userStoreInstance.selectedTask
 
   return (
-    <div className="flex flex-row px-5 py-5 space-x-8">
+    <div
+      className={classNames('flex flex-row px-5 py-5 space-x-8', {
+        'bg-light-2 dark:bg-dark-2': selected,
+      })}
+    >
       <Checkbox taskId={task._id} status={task.status} />
       <h3
-        onClick={onClick}
-        className={classNames(
-          'grow overflow-x-auto scrollbar cursor-pointer text-lg md:text-base',
-          {
-            'font-bold': selected,
-          }
-        )}
+        onDoubleClick={handleDoubleClick}
+        onClick={handleClick}
+        className="grow overflow-x-auto scrollbar cursor-pointer text-lg md:text-base"
       >
         {task.title}
       </h3>
