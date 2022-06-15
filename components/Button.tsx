e615424616b995d89819164e64825110ecdb5924
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-15 10:57:57 +0700

    🚧 Fix button to recieve other props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,27 @@
 import { Icon } from '@iconify/react'
 
 export interface IButtonProps {
-  onClick: () => void
+  onClick?: () => void
   text: string
   icon: string
+  className?: string
+  type?: 'button' | 'submit'
 }
 
-const Button = ({ onClick, text, icon }: IButtonProps) => {
+const Button = ({
+  onClick,
+  text,
+  icon,
+  className,
+  type = 'button',
+}: IButtonProps) => {
   return (
     <button
-      className="m-auto bg-light-4 dark:bg-dark-3 py-4 px-5 rounded-lg text-light-1 dark:text-dark-1 text-size-xl"
+      className={`bg-light-4 dark:bg-dark-3 py-4 px-5 rounded-lg text-light-1 dark:text-dark-1 text-size-xl hover:shadow-lg ${className}`}
       onClick={onClick}
+      type={type}
     >
-      <div className="flex">
+      <div className="flex space-x-1">
         <h3>{text}</h3>
         <Icon icon={icon} className="text-2xl" />
       </div>
