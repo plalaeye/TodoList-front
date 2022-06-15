f77dfeeb0c994233caf231b8448a64c607459610
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-15 09:56:50 +0700

    🚧 Complete `Login` component

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,27 +1,54 @@
 import { useState } from 'react'
+import userStoreInstance, { userStore } from '../contexts/UserStore'
 import Button from './Button'
-import Input from './Input'
+import Router from 'next/router'
+import { observer } from 'mobx-react'
+import { login, register } from '../api/AccountFunction'
+import { IUser } from '../interfaces/User'
 
-const Login = () => {
-  const [name, setName] = useState('')
+const Login = observer(() => {
+  const [name, setName] = useState<string>('')
 
-  const onClick = () => {
-    console.log('click')
+  const onClick = async () => {
+    if (!name || name === '') {
+      alert('Please enter your name')
+      return
+    }
+
+    const data = await login(name)
+    if (data) {
+      userStoreInstance.setUser(data)
+      Router.replace('/')
+    } else {
+      let message =
+        "This account doesn't exist.\nDo you wish to create a new account?"
+      if (confirm(message)) {
+        const data: IUser | undefined = await register(
+          name,
+          userStoreInstance.theme
+        )
+        if (data) {
+          userStoreInstance.setUser(data)
+          Router.replace('/')
+        } else {
+          alert('Username already exists')
+        }
+      }
+    }
   }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value)
-    console.log(name)
   }
 
   return (
     <>
       <div className="h-screen flex justify-center items-center bg-light-1 dark:bg-dark-1">
-        <div className="flex-column w-fit h-fit justify-items-center text-center space-y-6 p-10 bg-light-2 dark:bg-dark-2 rounded-2xl shadow-lg">
+        <div className="flex-column w-4/5 md:w-fit h-fit justify-items-center text-center space-y-6 p-10 bg-light-2 dark:bg-dark-2 rounded-2xl shadow-lg">
           <h1 className="font-bold text-light-4 dark:text-dark-4 text-2xl">
             Please enter your name
           </h1>
-          <Input type="text" placeholder="something" onChange={onChange} />
+          <input type="text" placeholder="username" onChange={onChange} />
           <Button
             onClick={onClick}
             text="Continue"
@@ -31,6 +58,6 @@ const Login = () => {
       </div>
     </>
   )
-}
+})
 
 export default Login
