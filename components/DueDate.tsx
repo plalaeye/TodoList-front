858e226239083fe4a7d98b01c37f1f6119159bbc
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-16 01:20:58 +0700

    🚧 Fix `DueDate`
    Better color and add `noText` prop

--- components/DueDate.tsx
+++ components/DueDate.tsx
@@ -6,36 +6,39 @@ import { TaskStatus } from '../enum/taskStatus'
 interface IDueDateProps {
   dueDate: Date | undefined
   taskStatus: TaskStatus
+  noText?: boolean
 }
 
-const DueDate = observer(({ dueDate, taskStatus }: IDueDateProps) => {
-  const date = dueDate ? new Date(dueDate) : undefined
+const DueDate = observer(
+  ({ dueDate, taskStatus, noText = false }: IDueDateProps) => {
+    const date = dueDate ? new Date(dueDate) : undefined
 
-  const isOverdue =
-    date && date < new Date() && taskStatus !== TaskStatus.COMPLETED
+    const isOverdue =
+      date && date < new Date() && taskStatus !== TaskStatus.COMPLETED
 
-  let text = dueDate ? `${date?.toISOString().split('T')[0]}` : 'No DueDate'
+    let text = dueDate ? `${date?.toISOString().split('T')[0]}` : 'No DueDate'
 
-  return (
-    <div
-      className={classNames(
-        'flex flex-row items-center space-x-2 min-w-fit',
-        { 'text-light-red dark:text-dark-red': isOverdue },
-        { 'text-light-4 dark:text-dark-4': isOverdue }
-      )}
-    >
-      <div title={isOverdue ? 'Overdue' : 'DueDate'}>
-        <Icon
-          icon={classNames(
-            { 'material-symbols:date-range-rounded': !isOverdue },
-            { 'jam:triangle-danger-f': isOverdue }
-          )}
-          className="text-2xl"
-        />
+    return (
+      <div
+        className={classNames(
+          'flex flex-row items-center space-x-2 min-w-fit',
+          { 'text-light-red dark:text-dark-red': isOverdue && !noText },
+          { 'text-light-4 dark:text-dark-4': !isOverdue || noText }
+        )}
+      >
+        <div title={isOverdue ? 'Overdue' : 'DueDate'}>
+          <Icon
+            icon={classNames(
+              { 'material-symbols:date-range-rounded': !isOverdue },
+              { 'jam:triangle-danger-f': isOverdue }
+            )}
+            className="text-2xl"
+          />
+        </div>
+        {noText ? '' : <h5 className="text-sm font-semibold">{text}</h5>}
       </div>
-      <h5 className="text-sm font-semibold">{text}</h5>
-    </div>
-  )
-})
+    )
+  }
+)
 
 export default DueDate
