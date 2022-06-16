78913d4248cf24fa39537e544382f9ff12e02352
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-06-16 10:46:07 +0700

    🚧 Limit length of category name and make add categoty a form

--- components/AddCategoryPopup.tsx
+++ components/AddCategoryPopup.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import { observer } from 'mobx-react'
+import React from 'react'
 import { useState } from 'react'
 import popupStoreInstance from '../contexts/PopupStore'
 import userStoreInstance from '../contexts/UserStore'
@@ -11,22 +12,32 @@ const AddCategoryPopup = observer(() => {
 
   const isShow = !popupStoreInstance.isAddCategory
 
+  const formRef = React.createRef<HTMLFormElement>()
+
   const onClose = () => {
     setCategory('')
     popupStoreInstance.closeAddCategory()
+    formRef.current?.reset()
   }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCategory(e.target.value)
   }
 
-  const onClick = () => {
+  const onSubmit = () => {
     if (!category || category === '') {
       alert('Please enter your category')
       return
     }
+
+    if (category.length > 32) {
+      alert('Category name cannot be longer than 32 characters')
+      return
+    }
+
     userStoreInstance.addCategory(category)
     popupStoreInstance.closeAddCategory()
+    formRef.current?.reset()
   }
 
   return (
@@ -38,15 +49,19 @@ const AddCategoryPopup = observer(() => {
     >
       <div className="flex flex-col w-4/5 md:w-1/3 h-fit rounded-xl shadow-lg bg-light-2 dark:bg-dark-2 p-8 space-y-5">
         <CloseButton onClick={onClose} />
-        <div className="flex flex-col justify-center items-center space-y-10">
+        <form
+          ref={formRef}
+          className="flex flex-col justify-center items-center space-y-10"
+          onSubmit={onSubmit}
+        >
           <h3 className="text-light-4 dark:text-dark-4 text-xl">
             Add new Category
           </h3>
           <input type="text" placeholder="Category" onChange={onChange} />
           <div>
-            <Button icon="fluent:add-16-filled" text="Add" onClick={onClick} />
+            <Button icon="fluent:add-16-filled" text="Add" type="submit" />
           </div>
-        </div>
+        </form>
       </div>
     </div>
   )
